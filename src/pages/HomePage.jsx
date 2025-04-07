31527abcee042c23106c852770fde24a043b7bbc
DEVASHISH <devashish@DEVASHISHs-MacBook-Air.local>
2025-04-07 12:04:26 +0530

    init

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+    return (
+      <main className="min-h-screen bg-white text-gray-900">
+        <h1 className="text-3xl font-bold text-center pt-20">CHAICODE Landing Page</h1>
+      </main>
+    );
+  }
+  export default HomePage;
+  
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+function NotFound() {
+  return (
+    <main className="h-screen flex items-center justify-center bg-black text-white">
+      <div>
+        <h1 className="text-6xl font-bold">404</h1>
+        <p className="text-xl">Oops! Lost in the code?</p>
+      </div>
+    </main>
+  );
+}
+export default NotFound;
